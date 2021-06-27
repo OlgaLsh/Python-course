ef4189e56f49ace7d7ac224c97488868acdd6918
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-06-27 23:18:00 +0300

    Vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,6 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -10,14 +9,37 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    a,b,c,k=len(plaintext),len(keyword),keyword,0
+    if b<a:
+      for i in range (a-b):
+        keyword += c[k]
+        k+=1
+        if k>=b:
+          k=0
+    m=0
+    keyword = keyword.upper()      
+    for i in plaintext:
+      b=keyword[m]
+      m+=1
+      a=ord(i)
+      shift = ord(b)-65
+      if shift == 0:
+        ciphertext += chr(a)
+  
+      elif a+shift>90 and 65<=a<=90:
+        ciphertext += chr(a + shift-26)
+      elif a+shift>122 and 97<=a<=122:
+        ciphertext += chr(a + shift-26)
+      elif (a+shift<=90 and 65<=a<=90) or (a+shift<=122 and 97<=a<=122):
+        ciphertext += chr(a + shift)
+      else:
+        ciphertext += chr(a)
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -26,5 +48,30 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    a,b,c,k=len(ciphertext),len(keyword),keyword,0
+    if b<a:
+      for i in range (a-b):
+        keyword += c[k]
+        k+=1
+        if k>=b:
+          k=0
+    m=0      
+    keyword = keyword.upper() 
+    for i in ciphertext:
+      b=keyword[m]
+      m+=1
+      a=ord(i)
+      shift = ord(b)-65
+      if shift == 0:
+        plaintext += chr(a)
+      
+      elif a - shift < 65 and 65 <= a <= 90:
+        plaintext += chr(a - shift + 26)
+      elif a - shift < 97 and 97 <= a <= 122:
+        plaintext += chr(a - shift +26 )
+      elif (a - shift >= 65 and 65 <= a <= 90) or (a - shift >= 97 and 97 <= a <= 122):
+        plaintext += chr(a - shift)
+      else:
+        plaintext += chr(a) 
+
     return plaintext
