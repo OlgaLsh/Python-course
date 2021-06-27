e8c4849b2757754d632c2b41ad38709ff07b0c87
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-06-27 23:17:10 +0300

    rsa

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -5,7 +5,6 @@ import typing as tp
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
-
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,20 +12,31 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    a=0
+    for i in range (1, n+1):
+        if n%i==0:
+            a+=1
+    if (a==2):
+        return bool(1)
+    else:
+        return bool(0)
     pass
 
 
 def gcd(a: int, b: int) -> int:
     """
     Euclid's algorithm for determining the greatest common divisor.
-
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
+    while (a!=0 and b!=0):
+        if a>b:
+            a=a%b
+        else:
+            b=b%a
+    return (a+b)
     pass
 
 
@@ -34,11 +44,16 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
-
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
+    if (phi==1):
+        return (0)
+    else:
+        d=0
+        while (d%(phi)!=1):
+            d=d+e
+        return (d//e)    
     pass
 
 
@@ -49,10 +64,9 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
-
+    n=p*q
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi=(p-1)*(q-1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
